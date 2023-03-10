eac6ee270e7a58e5424c3a0e6c7887bcd899f069
Cerjo-axe <sl.jr1599@gmail.com>
2023-03-09 21:40:08 -0300

    started usercontroller tests

---
+++ Application/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+using Domain.Interfaces;
+using DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _service;
+
+    public UsersController(IUserService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register([FromBody] RegisterDTO user)
+    {
+        try
+        {
+            var exists = await _service.CheckUserExists(user.Email);
+            if (!exists)
+            {
+                return BadRequest();
+            }
+
+            var result = await _service.Register(user);
+            return Ok();
+        }
+        catch (FluentValidation.ValidationException)
+        { 
+            return BadRequest();
+        }
+        
+    }
+}
